cb7ad0a5eee34a3c4f70e71fd8e11198ebf1c503
LeOzInNDF <leo_albano@hotmail.com>
2023-02-19 04:13:17 -0300

    Update code

--- DiceRoller/app/src/main/java/br/com/cyberjet/MainActivity.kt
+++ DiceRoller/app/src/main/java/br/com/cyberjet/MainActivity.kt
@@ -3,19 +3,20 @@ package br.com.cyberjet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countButton: Button = findViewById(R.id.countup_button)
-        countButton.setOnClickListener { countUp() }
+
 
     }
     private fun rollDice(){
@@ -23,21 +24,21 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
-    private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
 
-        if(resultText.text == "Hello World!"){
-            resultText.text = "1"
-        } else {
-            var resultInt = resultText.toString().toInt()
 
-            if (resultInt <6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableResource)
+
     }
-}
+
+
+            }
+
+
