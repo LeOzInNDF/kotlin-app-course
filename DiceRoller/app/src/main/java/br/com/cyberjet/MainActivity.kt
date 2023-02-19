3220c637b20e9a8778ae1f55511c29d67ed4dae2
LeOzInNDF <leo_albano@hotmail.com>
2023-02-19 02:50:35 -0300

    Update code

--- DiceRoller/app/src/main/java/br/com/cyberjet/MainActivity.kt
+++ DiceRoller/app/src/main/java/br/com/cyberjet/MainActivity.kt
@@ -2,10 +2,42 @@ package br.com.cyberjet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.countup_button)
+        countButton.setOnClickListener { countUp() }
+
+    }
+    private fun rollDice(){
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if(resultText.text == "Hello World!"){
+            resultText.text = "1"
+        } else {
+            var resultInt = resultText.toString().toInt()
+
+            if (resultInt <6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 }
