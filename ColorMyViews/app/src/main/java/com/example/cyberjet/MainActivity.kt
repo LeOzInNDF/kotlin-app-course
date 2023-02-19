73fda8892c49db643f0518e89ecc75a62f0b3acd
LeOzInNDF <leo_albano@hotmail.com>
2023-02-19 19:37:28 -0300

    Finalization app ColorMyViews

--- ColorMyViews/app/src/main/java/com/example/cyberjet/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/cyberjet/MainActivity.kt
@@ -1,32 +1,31 @@
 package com.example.cyberjet
 
+
+
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+
+/**
+ * The colorMyViews app demonstrates how to use a ConstraintLayout using
+ * the Layout Editor.
+ */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setListeners()
     }
 
-    private fun makeColored(view: View) {
-        when (view.id) {
-            // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
-        }
-
-    }
-
+    /**
+     * Attaches listeners to all the views.
+     */
     private fun setListeners() {
 
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
@@ -37,19 +36,49 @@ class MainActivity : AppCompatActivity() {
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
 
-            val clickableViews: List<View> =
-                listOf(
-                    boxOneText, boxTwoText, boxThreeText,
-                    boxFourText, boxFiveText, rootConstraintLayout
-                )
-            for (item in clickableViews) {
-                item.setOnClickListener { makeColored(it) }
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
+            )
 
-            }
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
 
 
         }
+
     }
 
+    /**
+     * Sets the background color of a view depending on it's resource id.
+     * This is a way of using one click handler to do similar operations on a
+     * group of views.
+     */
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
 
